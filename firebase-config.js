8e8c4cde6ed2abf277a52f50059198ee269d49a1
vhyb69bdf2-droid <vhyb69bdf2@privaterelay.appleid.com>
2026-06-30 20:25:47 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.CIPHERROOM_FIREBASE = {
+  apiKey: "AIzaSyDSqhETu2ATt9VfnMPVqRIf4zXVfw_4rWQ",
+  authDomain: "cipherroom-5fd37.firebaseapp.com",
+  databaseURL: "https://cipherroom-5fd37-default-rtdb.firebaseio.com",
+  projectId: "cipherroom-5fd37",
+  storageBucket: "cipherroom-5fd37.firebasestorage.app",
+  messagingSenderId: "582542293003",
+  appId: "1:582542293003:web:ffe59185f899d35d83af0e",
+  measurementId: "G-0892J0D9HE"
+};
